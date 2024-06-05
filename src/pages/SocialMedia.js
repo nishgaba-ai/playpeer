79602f6d33aa4e8c903fa181ac23d4a0077071f8
unknown <smartdesk2015@gmail.com>
2024-06-05 13:47:13 +0530

    icons

--- src/pages/SocialMedia.js
+++ src/pages/SocialMedia.js
@@ -1,15 +1,126 @@
+// import React from "react";
+// import x from "../assets/images/xx.png"
+// import telegram from "../assets/images/tele.png"
+// import discord from "../assets/images/discordd.png"
+// import {
+//   Image,
+//   Flex,
+//   Text,
+//   Heading,
+//   Container,
+// } from "@chakra-ui/react";
+// import "./SocialMedia.css";
+// import { Link } from "react-router-dom";
+
+// function SocialMedia() {
+//   const handleClick = (url) => {
+//     window.open(url, '_blank');
+//   };
+//   return (
+//     <div style={{ width: "100%" }}>
+//       <Flex
+//         gap={{ base: "33px", sm: "66px" }}
+//         zIndex={7}
+//         w={{ md: "93%", base: "100%" }}
+//         position="relative"
+//         flexDirection="column"
+//         alignItems="center"
+//         p={{ md: "", base: "20px" }}
+//       >
+//         <Container px={{ md: "113px", base: "20px" }}>
+//           <Flex
+//             gap="17px"
+//             flexDirection="column"
+//             alignItems="center"
+//             px="5px"
+//             style={{ width: "100%" }}
+//           >
+//             <Heading
+//               letterSpacing="-3.24px"
+//               w={{ md: "206%", base: "100%" }}
+//               style={{
+//                 color: "white",
+//                 textAlign: "center",
+//               }}
+//             >
+//               Connect Your Social Media
+//             </Heading>
+//             <Text
+//               size="xl"
+//               letterSpacing="-0.79px"
+//               textAlign="center"
+//               w={{ md: "86%", base: "100%" }}
+//               lineHeight="31px"
+//               style={{ color: "white" }}
+//             >
+//               Enjoy customizable lists, team work tools, and smart tracking all
+//               in one place. Set tasks, get reminders, and see your progress
+//               simply and quickly.
+//             </Text>
+//           </Flex>
+//         </Container>
+//         <Flex
+//           gap="25px"
+//           flexDirection="row"
+//           flexWrap="wrap"
+//           justifyContent="center"
+//           width="95%"
+       
+//         >
+        
+//             <Image
+//             src={x}
+//             alt="Twitter"
+//             onClick={() => handleClick(`https://x.com/Peer2playAI`)}
+//             style={{
+//               height: "auto",
+//               width: "20%",
+//               cursor :"pointer"
+//             }}
+//           />
+         
+//           <Image
+//             src={discord}
+//             alt="Discord"
+//             onClick={() => handleClick(`https://discord.gg/fN4MFFxC`)}
+//             style={{
+//               height: "auto",
+//               width: "20%",
+//               cursor :"pointer"
+             
+//             }}
+//           />
+//           <Image
+//             src={telegram}
+//             alt="Telegram"
+//             // onClick={() => handleClick('')}
+//             style={{
+//               height: "auto",
+//               width: "22%",
+//               cursor :"pointer"
+              
+//             }}
+//           />
+//         </Flex>
+//       </Flex>
+//     </div>
+//   );
+// }
+
+// export default SocialMedia;
+
+
 import React from "react";
+import x from "../assets/images/xx.png";
+import telegram from "../assets/images/tele.png";
+import discord from "../assets/images/discordd.png";
 import {
   Image,
-  Button,
-  Input,
   Flex,
   Text,
   Heading,
-  IconButton,
-  SimpleGrid,
   Container,
-  Box,
+  Link,
 } from "@chakra-ui/react";
 import "./SocialMedia.css";
 
@@ -17,7 +128,6 @@ function SocialMedia() {
   return (
     <div style={{ width: "100%" }}>
       <Flex
-        // mt="114px"
         gap={{ base: "33px", sm: "66px" }}
         zIndex={7}
         w={{ md: "93%", base: "100%" }}
@@ -25,7 +135,6 @@ function SocialMedia() {
         flexDirection="column"
         alignItems="center"
         p={{ md: "", base: "20px" }}
-        // style={{ marginTop: "-794px" }}
       >
         <Container px={{ md: "113px", base: "20px" }}>
           <Flex
@@ -60,24 +169,53 @@ function SocialMedia() {
           </Flex>
         </Container>
         <Flex
-          gap="16px"
-          alignSelf="stretch"
-          flexDirection={{ md: "row", base: "column" }}
+          gap="25px"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="center"
+          width="95%"
+          // border= "3px solid red"
+          // height="200px"
         >
-          <Image
-            className="sm-window"
-            src="images/Frame_124.png"
-            h="15%"
-            w="56%"
-            ml="26%"
-          />
-          <Image
-            className="sm-mobile"
-            src="images/Frame_124.png"
-            h="77px"
-            w="85%"
-            m="auto"
-          />
+          <Link href="https://x.com/Peer2playAI" isExternal>
+            <Image
+              src={x}
+              alt="Twitter"
+              style={{
+                cursor: "pointer",
+                height: "auto",
+                maxWidth: "200px",
+                width: "100%",
+                margin: "10px",
+              }}
+            />
+          </Link>
+          <Link href="https://discord.gg/fN4MFFxC" isExternal>
+            <Image
+              src={discord}
+              alt="Discord"
+              style={{
+                cursor: "pointer",
+                height: "auto",
+                maxWidth: "200px",
+                width: "100%",
+                margin: "10px",
+              }}
+            />
+          </Link>
+          <Link href="" isExternal>
+            <Image
+              src={telegram}
+              alt="Telegram"
+              style={{
+                cursor: "pointer",
+                height: "auto",
+                maxWidth: "200px",
+                width: "100%",
+                margin: "10px",
+              }}
+            />
+          </Link>
         </Flex>
       </Flex>
     </div>
@@ -85,3 +223,4 @@ function SocialMedia() {
 }
 
 export default SocialMedia;
+
